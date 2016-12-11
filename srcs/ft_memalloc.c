8f75132bd29d18b627f183ac7da832fe15d8fdc9
Robert LUTT <ausdauerr@gmail.com>
2016-12-11 15:17:36 -0800

    ft_memalloc tested, not initializing memory to 0.

---
+++ srcs/ft_memalloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlutt <ausdauerr@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 02:11:28 by rlutt             #+#    #+#             */
+/*   Updated: 2016/12/11 02:14:45 by rlutt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		*ft_memalloc(size_t size)
+{
+	char *res;
+	res = NULL;
+	if (size < sizeof(long long))
+		res = ft_strnew(size);
+	else if (res == NULL)
+		return(NULL);
+	return((void*)res);
+}

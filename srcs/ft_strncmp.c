087949472e18f174cdd5f19ac7ab894f65fa5fee
Robert LUTT <ausdauerr@gmail.com>
2016-12-11 23:46:43 -0800

    ft_stncmp.c altered && tested

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -17,7 +17,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (n > 0)
 	{
 		if (*s1 != *s2)
-			return (*(unsigned const*)s1 - *(unsigned const *)s2);
+			return ((unsigned char)*s1 - (unsigned char)*s2);
 		else if (*s1 == '\0')
 			return (0);
 		s1++;

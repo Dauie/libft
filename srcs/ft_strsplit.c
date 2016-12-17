399129b16bd214ea2445fb9f4e70e826abdddd65
Robert LUTT <ausdauerr@gmail.com>
2016-12-16 23:52:11 -0800

    ft_strsplit.c and helper functions ft_nxtwrdlen.c && ft_cntwrds.c

--- srcs/ft_strsplit.c
+++ srcs/ft_strsplit.c
@@ -12,13 +12,44 @@
 
 #include "libft.h"
 
+char 	*ft_xtrctnxtwrd(char *str, char c, size_t *i)
+{
+	size_t y;
+	char *res;
+
+	y = 0;
+	res = ft_memalloc(ft_nxtwrdlen(&str[*i], c));
+	while (str[*i] != c )
+	{
+		res[y] = str[*i];
+		y++;
+		*i += 1;
+	}
+	res[y] = '\0';
+	return (res);
+}
+
 char		**ft_strsplit(char const *s, char c)
 {
+	char **res;
 	size_t i;
-	size_t y;
+	size_t ii;
+	size_t wcnt;
 
 	i = 0;
-	y = 0;
-	while(ft_iss)
-
+	ii = 0;
+	wcnt = ft_cntwrds((char *)s , c);
+	if (!(res = (char **)ft_memalloc(sizeof(char *) * wcnt)))
+		return (NULL);
+	while((char)s[i] == c)
+		i++;
+	while (s[i] && ii < wcnt)
+	{
+		res[ii] = ft_xtrctnxtwrd((char *)s, c, &i);
+		ii++;
+		while (s[i] == c && s[i])
+			i++;
+	}
+	res[ii] = NULL;
+	return (res);
 }

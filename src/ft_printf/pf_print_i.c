af755a072518c7640eeb7da946e2cbb652bd2622
Robert LUTT <ausdauerr@gmail.com>
2017-03-05 14:58:43 -0800

    testing update printing %i

--- src/ft_printf/pf_print_i.c
+++ src/ft_printf/pf_print_i.c
@@ -6,7 +6,7 @@
 /*   By: rlutt <rlutt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 19:19:24 by rlutt             #+#    #+#             */
-/*   Updated: 2017/03/05 13:00:42 by rlutt            ###   ########.fr       */
+/*   Updated: 2017/03/05 14:58:18 by rlutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 int			pf_print_i(attrib *ph, uiput *db)
 {
 	int		phi;
-	
+
 	phi = va_arg(db->ap, int);
 	ph->len = ft_numlen(phi, 10);
+	if (ph->sign == TRUE)
+		ph->width -= 1;
 	pf_putnbr(phi, ph, db);
 	if (ph->width)
 		pf_putpad_c(ph, db);

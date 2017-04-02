000c36ccd0d4a69795ea47d2a16accd40db7abf6
Robert LUTT <ausdauerr@gmail.com>
2017-04-02 14:27:25 -0700

    still testing sharp

--- src/ft_printf/pf_print_o.c
+++ src/ft_printf/pf_print_o.c
@@ -6,7 +6,7 @@
 /*   By: rlutt <rlutt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 19:19:24 by rlutt             #+#    #+#             */
-/*   Updated: 2017/04/02 14:24:46 by rlutt            ###   ########.fr       */
+/*   Updated: 2017/04/02 14:26:53 by rlutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ static int 		manage_oattrib(attrib *ph)
 		ph->width--;
 		ph->actn = TRUE;
 	}
-	if (ph->hash == TRUE && !ph->width && ph->wprc == FALSE)
+	if (ph->hash == TRUE && !ph->width && ph->wprc == FALSE && ph->phd.uimt != 0)
 		ph->actn = TRUE;
 	if (ph->algn == TRUE && ph->zero == TRUE)						//need
 		ph->zero = FALSE;

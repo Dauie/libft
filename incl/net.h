13cde8a765c6158c11987b48c8ef3f2244ace230
Robert LUTT <ausdauerr@gmail.com>
2018-07-29 17:48:43 -0700

    adding a few definitions to net.h

--- incl/net.h
+++ incl/net.h
@@ -6,7 +6,7 @@
 /*   By: rlutt <rlutt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/27 23:53:41 by rlutt             #+#    #+#             */
-/*   Updated: 2018/07/28 00:21:27 by rlutt            ###   ########.fr       */
+/*   Updated: 2018/07/28 02:04:43 by rlutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,11 @@
 #include <arpa/inet.h>
 #include <net/if.h>
 
+# define IPV4_HDRLEN 20
+# define IP_MAXPACKET 65535
+# define DOMAIN_NAME_LEN 253
 
-int		            ft_domtoip(char *domain, char *addrbuff, size_t addrbuffsz);
-int		            ft_gethstaddr(char *addrbuff, size_t buffsz);
+int		ft_domtoip(char *domain, char *addrbuff, size_t addrbuffsz);
+int		ft_gethstaddr(char *addrbuff, size_t buffsz);
 
 #endif

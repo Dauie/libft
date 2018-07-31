83bf5b5d2b3e0acd9085ef83a8e1d9b2709cc133
Robert LUTT <ausdauerr@gmail.com>
2018-07-31 14:45:42 -0700

    getiface addr will no longer block loopback interface from being assigned

--- src/net/ft_getifaceaddr.c
+++ src/net/ft_getifaceaddr.c
@@ -7,8 +7,7 @@ static struct ifaddrs	*find_iface(struct ifaddrs *addrs, char *ifacename)
 		if (addrs->ifa_addr && (addrs->ifa_flags & IFF_UP))
 		{
 			if (addrs->ifa_addr->sa_family == AF_INET &&
-				((struct sockaddr_in *)addrs->ifa_addr)->sin_addr.s_addr != htonl(2130706433UL) &&
-					ft_strncmp(addrs->ifa_name, ifacename, ft_strlen(ifacename)) == 0)
+					(ft_strncmp(addrs->ifa_name, ifacename, ft_strlen(ifacename)) == 0))
 				return (addrs);
 		}
 		addrs = addrs->ifa_next;
@@ -16,7 +15,7 @@ static struct ifaddrs	*find_iface(struct ifaddrs *addrs, char *ifacename)
 	return (NULL);
 }
 
-int		ft_getifaceip(char *ifacename, char *addrbuff, size_t buffsz)
+int		ft_getifaceaddr(char *ifacename, char *addrbuff)
 {
 	struct ifaddrs	*addrs;
 	struct ifaddrs	*src;
@@ -24,7 +23,7 @@ int		ft_getifaceip(char *ifacename, char *addrbuff, size_t buffsz)
 	if (getifaddrs(&addrs) != 0 || !(src = find_iface(addrs, ifacename)))
 		return (FAILURE);
 	inet_ntop(AF_INET, &((struct sockaddr_in *) src->ifa_addr)->sin_addr,
-			  addrbuff, (socklen_t)buffsz);
+			  addrbuff, INET_ADDRSTRLEN);
 	freeifaddrs(addrs);
 	return (SUCCESS);
 }
